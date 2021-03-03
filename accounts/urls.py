073cfaaf7a0a7812a88e6fb58227fb2a22a4ff62
Vladimir <45636169+softmasterlab@users.noreply.github.com>
2021-03-03 19:30:11 +0200

    Доработана система авторизации

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import reg, entry, logout
-
+from .views import sign_up, sign_in, sign_out, profile, ajax_reg
 
 urlpatterns = [
-    path('reg', reg),
-    path('entry', entry),
-    path('logout', logout),
+    path('sign_up', sign_up),
+    path('sign_in', sign_in),
+    path('sign_out', sign_out),
+    path('profile', profile),
+    path('ajax_reg', ajax_reg)
 ]
